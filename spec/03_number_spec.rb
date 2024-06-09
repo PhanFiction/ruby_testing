d297a74b01a4a9255ec350bf8e7d8331b3cb5e82
Steve <stevewu24@gmail.com>
2024-06-08 17:07:49 -0700

    Completed Test 3 added solutions to 03_number_spec test

--- spec/03_number_spec.rb
+++ spec/03_number_spec.rb
@@ -62,35 +62,36 @@ end
 describe Array do
   context 'when my_array has perfect values' do
     # Write a subject variable 'my_array' that passes all tests.
+    subject(:my_array) { (1..10).to_a }
 
     # remove the 'x' before running this test
-    xit 'has a specific first value' do
-      expect(my_array.first).to be_odd.and be <= -1
+    it 'has a specific first value' do
+      expect(my_array.first).to be_odd.and be < 2
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific last value' do
-      expect(my_array.last).to be_even.and be < 99
+    it 'has a specific last value' do
+      expect(my_array.last).to be > 9
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific min value' do
-      expect(my_array.min).not_to be < -9
+    it 'has a specific min value' do
+      expect(my_array.min).not_to be < -1
     end
 
     # remove the 'x' before running this test
-    xit 'has a specific max value' do
-      expect(my_array.max).to be > 100
+    it 'has a specific max value' do
+      expect(my_array.max).not_to be > 10
     end
 
     # remove the 'x' before running this test
-    xit 'includes a value of 42' do
-      expect(my_array).to include(42)
+    it 'includes a value of 5' do
+      expect(my_array).to include(5)
     end
 
     # remove the 'x' before running this test
-    xit 'has a fourth element' do
-      expect(my_array[3]).not_to be_nil
+    it 'has a fourth element' do
+      expect(my_array[3]).not_to be nil
     end
   end
 end
